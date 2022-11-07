e714178f48260c8a05d9e8f41a72a3b28573a45a
TomTsai <tomp0106@yahoo.com.tw>
2022-11-07 16:02:51 +0800

    1.報告編碼定義為UTF-8輸出

--- main.py
+++ main.py
@@ -26,6 +26,13 @@ MaterialSeacher=SeleniumAction()
 print('進行登入作業....')
 LoginStus=MaterialSeacher.LoginAction()
 
+
+ReportFileName='SearchResult.html'
+
+if os.path.exists(ReportFileName):
+    os.remove(ReportFileName)
+
+
 for MaterialSNFileName in MaterialSNFileNameList:
 
     f=open('./BOM/'+MaterialSNFileName,'r')
@@ -50,11 +57,35 @@ for MaterialSNFileName in MaterialSNFileNameList:
                     if x ==ErrRetryTimes+1:
                         print('')
 
-
+        #匯出報告
         if len(SeachResultList)!=0:
-            f = open('{}_SeachResult.csv'.format(ProductGroupName), 'w+')
+            f = open('{}_SeachResult.csv'.format(ProductGroupName), 'w+',encoding='utf8')
             for SeachResultItem in SeachResultList:
                 f.write(SeachResultItem + '\n')
             f.close()
+
+
+
+
+            f2=open(ReportFileName,'a+',encoding='utf8')
+            f2.write('<h2>{}</h2>\n<table border="1" cellpadding="10">'.format(ProductGroupName))
+            f2.write('''    
+                            <tr style="background-color:#DCE6F1;">
+                                <th>料號</th>
+                                <th>品項</th>
+                                <th>公司庫存數量</th>
+                            </tr>''')
+            for SeachResultItem in SeachResultList:
+                SeachResultItems = SeachResultItem.split(',')
+                f2.write('''
+                              <tr>
+                                <th>{}</th>
+                                <th>{}</th>
+                                <th>{}</th>
+                              </tr>
+                         '''.format(SeachResultItems[0],SeachResultItems[1],SeachResultItems[2]))
+            f2.write('</table>')
+            f2.close()
+
 
 MaterialSeacher.CloseBrower()
